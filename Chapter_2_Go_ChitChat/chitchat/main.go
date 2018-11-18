25d5330df102383676e8d0c538ba3b26e26c845e
Geir Haugom <ghagit@haugom.org>
2018-11-18 19:39:12 +0100

    Add tls setup

--- Chapter_2_Go_ChitChat/chitchat/main.go
+++ Chapter_2_Go_ChitChat/chitchat/main.go
@@ -44,5 +44,5 @@ func main() {
 		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
 		MaxHeaderBytes: 1 << 20,
 	}
-	server.ListenAndServe()
+	server.ListenAndServeTLS("ssl/gwp_chain.pem", "ssl/gwp-key.pem")
 }
