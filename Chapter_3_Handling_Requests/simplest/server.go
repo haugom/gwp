61da7e25a33e95144904cc45e25a631b9f13f304
Geir Haugom <ghagit@haugom.org>
2018-11-18 19:46:47 +0100

    Add two handlers

--- Chapter_3_Handling_Requests/simplest/server.go
+++ Chapter_3_Handling_Requests/simplest/server.go
@@ -5,17 +5,25 @@ import (
 	"net/http"
 )
 
-type MyHandler struct{}
+type HelloHandler struct{}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello!")
+}
+
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "World!")
 }
 
 func main() {
-	handler := MyHandler{}
+	hello := HelloHandler{}
+	world := WorldHandler{}
 	server := http.Server{
 		Addr: "127.0.0.1:8081",
-		Handler: &handler,
 	}
+	http.Handle("/hello", &hello)
+	http.Handle("/world", &world)
 	server.ListenAndServe()
 }
