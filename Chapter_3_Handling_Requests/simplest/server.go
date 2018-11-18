78e904da911f7188eeb811b3cd5a1a9af59a057f
Geir Haugom <ghagit@haugom.org>
2018-11-18 19:55:03 +0100

    Test http.HandlerFunc

--- Chapter_3_Handling_Requests/simplest/server.go
+++ Chapter_3_Handling_Requests/simplest/server.go
@@ -21,14 +21,20 @@ func goHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Go!")
 }
 
+func programming(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "programming")
+}
+
 func main() {
 	hello := HelloHandler{}
 	world := WorldHandler{}
+	programming := http.HandlerFunc(programming)
 	server := http.Server{
 		Addr: "127.0.0.1:8081",
 	}
 	http.Handle("/hello", &hello)
 	http.Handle("/world", &world)
+	http.Handle("/programming", programming)
 	http.HandleFunc("/go", goHandler)
 	server.ListenAndServe()
 }
