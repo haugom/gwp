eb20bfe62c821f2915b14a3d85393ae971315f83
Geir Haugom <ghagit@haugom.org>
2018-12-17 23:02:59 +0100

    Mental processing to prepare for the next task (work in progress)

--- oauth/ch-5-ex-1/authorizationServer.go
+++ oauth/ch-5-ex-1/authorizationServer.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gorilla/handlers"
 	"github.com/justinas/alice"
 	"html/template"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -18,20 +19,39 @@ type client struct {
 	RedirectURIS redirectURIS `json:"redirect_uris"`
 }
 
-var Clients []client
+type clients map[string]client
+
+type appData struct {
+	clients * clients
+}
+
+var myClients clients
+
+type myError struct {
+	error string `json:"error"`
+}
+
+var currentError myError
 
 func main() {
 
-	Clients = append(Clients,
-		client{
+	currentError = myError{}
+	myClients = clients{
+		"oauth-client-1": {
 			"oauth-client-1",
 			"oauth-client-secret-1",
 			redirectURIS{"http://localhost:9000/callback"},
-		})
+		},
+	}
+
+	appData := appData{clients:&myClients}
+
+	log.Println(appData.clients)
 
 	indexHandler := http.HandlerFunc(index)
 	errorHandler := http.HandlerFunc(error)
 	approveHandler := http.HandlerFunc(approve)
+	authorizeHandler := http.HandlerFunc(appData.authorize)
 
 	stdChain := alice.New(myLoggingHandler, dumpRequest)
 
@@ -39,6 +59,7 @@ func main() {
 	mux.Handle("/", stdChain.Then(indexHandler))
 	mux.Handle("/error", stdChain.Then(errorHandler))
 	mux.Handle("/approve", stdChain.Then(approveHandler))
+	mux.Handle("/authorize", stdChain.Then(authorizeHandler))
 
 	server := http.Server{
 		Addr: "127.0.0.1:9002",
@@ -46,6 +67,15 @@ func main() {
 	}
 	server.ListenAndServe()
 
+}
+
+func (c *appData) getClient(ClientId string) client {
+	theClient, ok := (*c.clients)[ClientId]
+	if ok {
+		return theClient
+	} else {
+		return client{}
+	}
 }
 
 func dumpRequest(h http.Handler) http.Handler {
@@ -73,4 +103,12 @@ func error(writer http.ResponseWriter, request *http.Request) {
 func approve(writer http.ResponseWriter, request *http.Request) {
 	templates := template.Must(template.ParseFiles("templates/authorizationServer/approve.html"))
 	templates.ExecuteTemplate(writer, "approve.html", nil)
+}
+
+func (c *appData) authorize(writer http.ResponseWriter, request *http.Request) {
+	clientId := c.getClient(request.URL.Query().Get("client_id"))
+	if len(clientId.ClientID) == 0 {
+		request.Context()
+	}
+
 }
